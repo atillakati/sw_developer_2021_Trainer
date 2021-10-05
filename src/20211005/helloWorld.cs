ad17a5c713b2de50b28ad79b45c109aa12558cc4
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-10-05 21:38:22 +0200

    Ubungen vom 05.10.2021 => KickOff

---
+++ src/20211005/helloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Wifi.HelloWorld
+{
+	public class Application
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Hello World..");
+			Console.ReadLine();
+		}
+	}
+}
