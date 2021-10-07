7de143d158f74ff43a421e9f37825681e8ecf14f
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-10-07 21:29:35 +0200

    Anatomie C# Applikation

---
+++ src/20211007/AusgabenGL/AusgabenGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AusgabenGL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Heute ist der " + DateTime.Now);
+            Console.WriteLine("Schönen Tag!");
+        }
+    }
+
+}
