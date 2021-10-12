bd18d5626592ce92601b92fa6e421580b875fc72
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-10-12 17:30:38 +0200

    Erstes Besipiel zum Thema Ausgaben in der Console

---
+++ src/20211012/Ausgaben_GL/Ausgaben_GL/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ausgaben_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Mein Name ist Gandalf!\n\n");            
+            ////Console.WriteLine("Mein Alter:" + 150);
+            //Console.WriteLine("Ich bin " + 150 + " Jahre alt.");
+
+            //Console.WriteLine("Hallo\nzusammen!");
+            //Console.WriteLine("\tHallo\nzusammen!");
+            //Console.WriteLine("Hallo\n\tzusammen!");
+            //Console.WriteLine("\t\tHallo\nzusammen!\a");
+
+            /**********************
+             * 
+             *          Name: [euren Namen]
+             *  Geburts Jahr: [euer Gburtsjahr]
+             * 
+             * Das halte ich von C#:
+             *          [hier eine Message zu C# ausgeben!]
+             * 
+             * */
+
+            Console.WriteLine("\n\t  Name: Atilla Kati");
+            Console.WriteLine("  Geburts Jahr: 1978");
+            Console.WriteLine("\nDas halte ich von C#:");
+            Console.Write("\t  Why do Java developers wear glasses?");
+            Console.ReadKey(true);
+            Console.WriteLine("   Because they don't C#");
+        }
+    }
+}
