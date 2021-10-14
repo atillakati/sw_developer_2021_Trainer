0ced707c1faa4218691b8cea01cbe087d20671b4
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-10-14 21:27:23 +0200

    Grundlagen Exception try-catch

---
+++ src/20211014/Exceptions_GL/Exceptions_GL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exceptions_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int geburtsJahr = 0;
+            int alter = 0;
+            bool isInputValid = false;
+
+
+            Console.Write("Bitte Geburtsjahr eingeben: ");
+
+            try
+            {              
+                geburtsJahr = int.Parse(Console.ReadLine());
+                isInputValid = true;
+            }
+            catch
+            {
+                Console.WriteLine("Ups! Leider ist was schief gegangen!");
+                isInputValid = false;
+            }
+
+            if (isInputValid == true)
+            {
+                alter = DateTime.Now.Year - geburtsJahr;
+
+                Console.WriteLine("Du bist " + alter + " Jahre alt.");
+            }
+        }
+    }
+}
