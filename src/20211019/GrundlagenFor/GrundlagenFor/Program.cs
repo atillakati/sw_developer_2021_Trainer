4cf0fecf98b4296d055fe8dc3a7ab389800e8b75
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-10-19 20:55:18 +0200

    Grundlagen Iterationen

---
+++ src/20211019/GrundlagenFor/GrundlagenFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrundlagenFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 10;
+
+            //Initialisierung; Abbruchbedingung; Reinitialisierung
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Ausgabe mit {i}");
+                Console.WriteLine($"Ausgabe mit {i}");
+                Console.WriteLine($"Ausgabe mit {i}");
+            }                                    
+        }
+    }
+}
---
+++ src/20211019/GrundlagenWhile/GrundlagenWhile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrundlagenWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl = 30;
+
+
+            while(zahl < DateTime.Now.Second)
+            {
+                Console.WriteLine($"while: {DateTime.Now.Second}");
+                Thread.Sleep(1000);
+            }
+           
+
+            do
+            {
+                Console.WriteLine($"do-while: {DateTime.Now.Second}");
+                Thread.Sleep(1000);
+            }
+            while (zahl < DateTime.Now.Second);
+
+
+        }
+    }
+}
