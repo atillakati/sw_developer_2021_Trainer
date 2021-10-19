80ae7403d873cb39923ed47c7dce4e29405571c0
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-10-19 21:29:41 +0200

    Methoden Grundlagen

---
+++ src/20211019/EingabeMitWiederholung/EingabeMitWiederholung/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EingabeMitWiederholung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 0;
+            bool isInputValid = false;
+
+
+            do
+            {
+                try
+                {
+                    Console.Write("Bitte das Alter eingeben: ");
+                    alter = int.Parse(Console.ReadLine());
+
+                    isInputValid = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Ungültige Eingabe.");
+                    isInputValid = false;
+                }
+            }
+            while (!isInputValid);
+
+            Console.WriteLine($"Sie sind {alter} Jahre alt.");
+        }
+    }
+}
---
+++ src/20211019/MethodenGL/MethodenGL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodenGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHelloWorld();
+
+            string name = "Gandalf";
+
+            PrintMessage("Test");
+            PrintMessage(name);
+            PrintMessage(Console.ReadLine());
+
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        //Signatur
+        //RückgabeTyp MethodenBezeichnung(Parameter)
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
