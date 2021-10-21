5126d466fca967d8cf04e91edb6d65c6405cbcea
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-10-21 21:38:02 +0200

    Klassbibliothek & Übungsbeispiel

---
+++ src/20211021/Buchverwaltung_v1/Buchverwaltung_v1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buchverwaltung_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Die Applikation soll Buchdaten erfassen. Folgende Daten sollen 
+             * dabei berücksichtigt werden:
+             * 
+             *  - Titel
+             *  - Autor
+             *  - Erscheinungsjahr
+             *  - IBAN
+             *  - Preis
+             *  
+             *  Dabei gilt:
+             *  
+             *  1. Verwende und schreibe Methoden!
+             *  2. Realisiere fehlertolerante Eingaben!
+             *  
+             *  3. Speichere die Buchdaten in einer Datei, welcher so heisst wie der Titel
+             *     Bsp: Buch Titel = "Die unendliche Geschichte" => DieunendlicheGeschichte.book
+             *     
+             *  4. Baue das Programm so um, dass der User beliebig viele Bücher eingeben kann und jedes
+             *     Buch nach der Eingabe automatisch abgespeichert wird (als Datei).
+             
+             *  [Optional]
+             *  5. Alle gespeicherten Bücher sollen eingelesen und dargestellt werden.
+             *  */
+        }
+    }
+}
---
+++ src/20211021/Wifi.Tools.Solution/Wifi.ConsoleTools/UIHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.ConsoleTools
+{
+    public class UIHelper
+    {
+        /// <summary>
+        /// Prints a red colored message with (no automatic newline) to default Output-Stream on the console.
+        /// </summary>
+        /// <param name="message">The message which should displayed</param>        
+        public static void PrintColoredMessage(string message)
+        {
+            PrintColoredMessage(message, ConsoleColor.Red);
+        }
+
+        /// <summary>
+        /// Prints a colored message with (no automatic newline) to default Output-Stream on the console.
+        /// </summary>
+        /// <param name="message">The message which should displayed</param>
+        /// <param name="messageColor">The color the provided message should be displayed in.</param>        
+        public static void PrintColoredMessage(string message, ConsoleColor messageColor)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = messageColor;
+
+            Console.Write(message);
+
+            Console.ForegroundColor = oldColor;
+        }
+
+    }
+}
