1617fc7354ad5b96e783c684d71d1b520483d3ce
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-11-02 20:29:13 +0100

    Grundlagen Strukturen

---
+++ src/20211102/Strukturen_GL/Strukturen_GL/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strukturen_GL
+{
+    struct Book
+    {
+        public string Title;
+        public string Author;
+        public string Isbn;
+        public decimal Price;
+        public int PublishYear;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklaration
+            Book myBook;
+
+            //Erzeugung
+            myBook = new Book();
+
+            Book myOtherBook = new Book();
+
+            myOtherBook.Title = "TestTitel";
+            myOtherBook.PublishYear = 1980;
+
+            DisplayBook(myOtherBook);
+            DisplayBook(myBook);
+            myBook = ReadBookData();
+
+            Console.WriteLine(myBook);
+        }
+
+        static void DisplayBook(Book bookToDisplay)
+        {
+            Console.WriteLine(bookToDisplay.Title);
+            Console.WriteLine(bookToDisplay.Author);           
+            Console.WriteLine(bookToDisplay.PublishYear);            
+        }
+
+        static Book ReadBookData()
+        {
+            Book aNewBook = new Book();
+            //eingaben bla bla..
+
+            return aNewBook;
+        }
+    }
+}
