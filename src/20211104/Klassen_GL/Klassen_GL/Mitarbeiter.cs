29fef8c8ce94b92855388bfe1dc5b25437892a06
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-11-04 21:31:59 +0100

    Einstieg Klassen

---
+++ src/20211104/Klassen_GL/Klassen_GL/Mitarbeiter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen_GL
+{
+    class Mitarbeiter
+    {
+        public string Name;
+
+        public void ChangeName(string newName)
+        {
+            if (!string.IsNullOrEmpty(newName))
+            {
+                Name = newName;
+            }
+        }
+    }
+}
---
+++ src/20211104/Klassen_GL/Klassen_GL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanzierung
+            Mitarbeiter ma = new Mitarbeiter();
+
+            //Klasse / Object = Instanz            
+        }
+    }
+}
