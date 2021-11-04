4809729b56ae61b389b52a892209ec632eb9ffb1
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-11-04 21:11:28 +0100

    Grundlagen ENUM

---
+++ src/20211104/Enum_GL/Enum_GL/PowerState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_GL
+{
+    enum PowerState
+    {
+        On = 0,
+        Off = 1,
+        Standby = 2,
+        Hibernate = 3,
+        Exploded = 20
+    }
+}
---
+++ src/20211104/Enum_GL/Enum_GL/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklaration
+            PowerState status;
+
+            //Initialisierung
+            status = PowerState.Hibernate;
+
+            Console.WriteLine($"Wert von Status: {status}");
+
+            if(status == PowerState.Exploded)
+            {
+                return;
+            }
+            
+        }
+    }
+}
