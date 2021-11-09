d8dd13d4a08a20987ea46bfe7f2b2b6702eab9c7
Atilla@Wifi <User@DEV-DESKTOP-00>
2021-11-09 21:33:29 +0100

    OOP - Kapselung

---
+++ src/20211109/OOP_Grundlagen/OOP_Grundlagen/Mitarbeiter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Wifi.ConsoleTools.Misc;
+
+namespace OOP_Grundlagen
+{
+    class Mitarbeiter
+    {
+        private Guid _id;
+
+        private string _name;
+
+        private Gender _geschlecht;
+
+        private DateTime _birthday;
+
+
+        //std. Konstruktor
+        //public Mitarbeiter()
+        //{
+        //    Id = Guid.NewGuid();
+        //    Geschlecht = Gender.Undefined;
+        //    Name = "No Name_" + Id;
+        //}
+
+        //benutzerspezifische Konstruktor
+        public Mitarbeiter(string name, Gender geschlecht, DateTime birthday)
+        {
+            _id = Guid.NewGuid();
+            _geschlecht = geschlecht;
+            _name = name;
+            _birthday = birthday;
+        }
+
+        //benutzerspezifische Konstruktor
+        public Mitarbeiter(Guid id, string name, Gender geschlecht, DateTime birthday)
+        {
+            _id = id;
+            _geschlecht = geschlecht;
+            _name = name;
+            _birthday = birthday;
+        }
+
+        //Änderungs- und Zugriffsmethoden
+        //public string get_Name()
+        //{
+        //    return Name;
+        //}
+
+        //public Gender GetGeschlecht()
+        //{
+        //    return Geschlecht;
+        //}
+
+        //public Guid GetId()
+        //{
+        //    return Id;
+        //}
+
+        //public void SetId(Guid newId)
+        //{
+        //    if (newId != Guid.Empty)
+        //    {
+        //        Id = newId;
+        //    }
+        //}        
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }            
+        }
+
+        public Gender Geschlecht
+        {
+            get { return _geschlecht; }
+            set { _geschlecht = value; }
+        }
+
+        public int Alter
+        {
+            get { return DateTime.Now.Year - _birthday.Year; }
+        }
+
+        public void ChangeName(string newName)
+        {
+            if (!string.IsNullOrEmpty(newName))
+            {
+                _name = newName;
+            }
+        }
+
+        public void DisplayInfos()
+        {
+            Console.WriteLine($"    ID: [{_id}]");
+            Console.WriteLine($"  Name: {_name}");
+            Console.WriteLine($"Gender: {_geschlecht}");
+        }
+    }
+}
---
+++ src/20211109/OOP_Grundlagen/OOP_Grundlagen/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using Wifi.ConsoleTools.Misc;
+
+namespace OOP_Grundlagen
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Mitarbeiter ma = new Mitarbeiter("Gandalf", Gender.Male, new DateTime(1890, 5,20));
+            Mitarbeiter ma2 = new Mitarbeiter("Martha", Gender.Female, new DateTime(2001, 4, 10));
+
+            
+            //ma2.DisplayInfos();            
+
+            DisplayEmployeeId(ma);
+            DisplayEmployeeId(ma2);            
+
+            ma.DisplayInfos();                        
+        }
+
+        static void DisplayEmployeeId(Mitarbeiter ma)
+        {                       
+            Console.WriteLine($"ID: {ma.Id}");
+        }
+    }
+}
